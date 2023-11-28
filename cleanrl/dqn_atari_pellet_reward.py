49323ebc7d783a2622b1ac933c1f76355f60392c
Oliver Harold <oliverhsj@hotmail.dk>
2023-11-28 16:48:59 +0700

    Update dqn_atari_pellet_reward.py

--- cleanrl/dqn_atari_pellet_reward.py
+++ cleanrl/dqn_atari_pellet_reward.py
@@ -21,6 +21,7 @@ from stable_baselines3.common.atari_wrappers import (
     MaxAndSkipEnv,
     NoopResetEnv,
 )
+from gymnasium.wrappers import AtariPreprocessing
 from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.common.vec_env import VecNormalize
 from stable_baselines3.common.type_aliases import (
@@ -371,16 +372,17 @@ def make_env(env_id, seed, idx, capture_video, run_name):
             env = gym.make(env_id)
         env = gym.wrappers.RecordEpisodeStatistics(env)
 
-        env = NoopResetEnv(env, noop_max=30)
-        env = MaxAndSkipEnv(env, skip=4)
+        # env = NoopResetEnv(env, noop_max=30)
+        # env = MaxAndSkipEnv(env, skip=4)
         # env = EpisodicLifeEnv(env)
         # if "FIRE" in env.unwrapped.get_action_meanings():
         #     env = FireResetEnv(env)
         # env = ClipRewardEnv(env)
-        env = gym.wrappers.ResizeObservation(env, (84, 84))
-        env = gym.wrappers.GrayScaleObservation(env)
+        env = gym.wrappers.AtariPreprocessing(env, noop_max=0)
+        # env = gym.wrappers.ResizeObservation(env, (84, 84))
+        # env = gym.wrappers.GrayScaleObservation(env)
         env = gym.wrappers.FrameStack(env, 4)
-
+        
         env.action_space.seed(seed)
         return env
 
