810cb1ff241955b100ecc679bc11f0394724d9d9
Oliver Harold <oliverhsj@hotmail.dk>
2024-04-08 20:48:07 +0700

    Update ppo_atari_envpool_vae.py

--- scratchpads/ppo_atari_envpool_vae.py
+++ scratchpads/ppo_atari_envpool_vae.py
@@ -41,11 +41,11 @@ class Args:
     """the id of the environment"""
     total_timesteps: int = int(1e7)
     """total timesteps of the experiments"""
-    learning_rate: float = 1e-4
+    learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
     num_envs: int = 16
     """the number of parallel game environments"""
-    num_steps: int = 128
+    num_steps: int = 2048
     """the number of steps to run in each environment per policy rollout"""
     anneal_lr: bool = True
     """Toggle learning rate annealing for policy and value networks"""
