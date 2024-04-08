ebdf27c6a95c4764909232b8918340bb127aaf26
Oliver Harold <oliverhsj@hotmail.dk>
2024-04-08 18:36:51 +0700

    Update ppo_atari_envpool_vae.py

--- scratchpads/ppo_atari_envpool_vae.py
+++ scratchpads/ppo_atari_envpool_vae.py
@@ -53,7 +53,7 @@ class Args:
     """the discount factor gamma"""
     gae_lambda: float = 0.95
     """the lambda for the general advantage estimation"""
-    num_minibatches: int = 64
+    num_minibatches: int = 4
     """the number of mini-batches"""
     update_epochs: int = 4
     """the K epochs to update the policy"""
