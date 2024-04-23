5b9a3c1875cdc97db8e97f34bca8b88bcd557a0c
Oliver Harold <oliverhsj@hotmail.dk>
2024-04-23 11:19:44 +0700

    Update experiment_runner_doorkey.py

--- scratchpads/experiment_runner_doorkey.py
+++ scratchpads/experiment_runner_doorkey.py
@@ -10,7 +10,7 @@ import tyro
 class Args:
     track: bool = False
     """track the experiment"""
-    wandb_project_name: str = 'gridworld-rnd-hp-tuning'
+    wandb_project_name: str = 'gridworld-rnd-hp-tuning-no-temp'
     """track the experiment"""
     repeats: int = 1
     """number of times to repeat"""
@@ -24,7 +24,7 @@ class Args:
     """avaliable device ids"""
     include_noisy: bool = False
     """avaliable device ids"""
-    include_template: bool = True
+    include_template: bool = False
     """avaiable"""
     mode: str = 'hard'
     """difficulty"""
@@ -38,17 +38,17 @@ args = tyro.cli(Args)
 env_ids_and_tags = [
     # ('Visual/DoorKey5x5-Gridworld-v0' + ' --total-timesteps 2000000 --int-coef 0.2', 'doorkey5x5'),
     # ('Visual/DoorKey6x6-Gridworld-v0' + ' --total-timesteps 3000000 --int-coef 0.2', 'doorkey6x6'),
-    # ('Visual/DoorKey8x8-Gridworld-v0' + ' --total-timesteps 4000000 --int-coef 0.2', 'doorkey8x8'),
+    ('Visual/DoorKey8x8-Gridworld-v0' + ' --total-timesteps 2500000 --int-coef 0.2', 'doorkey8x8'),
     # ('Visual/DoorKey16x16-Gridworld-v0' + ' --total-timesteps 20000000 --int-coef 1.0 --ext-coef 4.0 --update_epochs 8', 'doorkey16x16'),
-    ('Visual/MultiRoomS10N6-Gridworld-v0'+ ' --cell_size 3 --total-timesteps 3000000 --int-coef 1.0 --update_epochs 8', 'multiroomS10N6'),
+    ('Visual/MultiRoomS10N6-Gridworld-v0'+ ' --cell_size 3 --total-timesteps 5000000 --int-coef 1.0 --update_epochs 8', 'multiroomS10N6'),
 ]
 
 noisy_env_ids_and_tags = [
     # ('Visual/NoisyDoorKey5x5-Gridworld-v0', 'doorkey5x5,noisy'),
     # ('Visual/NoisyDoorKey6x6-Gridworld-v0', 'doorkey6x6,noisy'),
-    # ('Visual/NoisyDoorKey8x8-Gridworld-v0' + ' --total-timesteps 5000000 --int-coef 0.01 --ext-coef 20 --update_epochs 8', 'doorkey8x8,noisy'),
-    ('Visual/NoisyDoorKey16x16-Gridworld-v0' + ' --total-timesteps 5000000 --int-coef 0.01  --ext-coef 4.0 --update-epochs 8', 'doorkey16x16,noisy'),
-    # ('Visual/NoisyMultiRoomS10N6-Gridworld-v0'+ ' --cell_size 5 --total-timesteps 5000000 --int-coef 0.01 --ext-coef 20 --update_epochs 8', 'multiroomS10N6,noisy'),
+    ('Visual/NoisyDoorKey8x8-Gridworld-v0' + ' --total-timesteps 5000000 --int-coef 0.01 --update_epochs 8', 'doorkey8x8,noisy'),
+    # ('Visual/NoisyDoorKey16x16-Gridworld-v0' + ' --total-timesteps 5000000 --int-coef 0.01  --update-epochs 8', 'doorkey16x16,noisy'),
+    ('Visual/NoisyMultiRoomS10N6-Gridworld-v0'+ ' --cell_size 3 --total-timesteps 5000000 --int-coef 0.01 --update_epochs 8', 'multiroomS10N6,noisy'),
 ]
 
 @dataclass(frozen=True)
@@ -130,8 +130,8 @@ if __name__ == '__main__':
         all_options.append(Option('include_fixed', ['fixed_seed'], ['--fixed'], ''))
         
 
-    # if args.include_template:
-    #     all_options.append(Option('include_template', ['template',None], ['--use-template', '--no-use-template'], ''))
+    if args.include_template:
+        all_options.append(Option('include_template', ['template',None], ['--use-template', '--no-use-template'], ''))
 
     if args.include_none_rnd:
         all_options.append(Option('include_rnd', ['base','rnd'], [0, 1], '--int-coef'))
@@ -156,21 +156,21 @@ if __name__ == '__main__':
     all_options.append(Option('external coef', [None, ] * len(ext_coef), ext_coef, '--ext-coef'))
     all_options.append(Option('entropy coef', [None, ] * len(ent_coef), ent_coef, '--ent-coef'))
     all_options.append(Option('learning rate', [None, ] * len(lr), lr, '--learning-rate'))
-    all_options.append(Option('include_template', ['template',], ['--use-template',], ''))
+    all_options.append(Option('include_template', [None, ], ['--no-use-template', ], ''))
     commands = construct_all_commands(base_cmd, all_options)
-    print(f' ===== No. experiments: {len(commands)} ===== ', *commands, sep='\n')
+    print(f' ===== No. experiments: {len(commands)} ===== ', *commands, f' ===== No. experiments: {len(commands)} ===== ', sep='\n')
 
 
-    # Using ThreadPoolExecutor to manage concurrent execution
-    with concurrent.futures.ThreadPoolExecutor(max_workers=args.max_workers) as executor:
-        # Map commands to future tasks
-        future_to_cmd = {executor.submit(run_command, cmd): cmd for cmd in commands}
-        # As each command completes, print its result
-        for future in concurrent.futures.as_completed(future_to_cmd):
-            cmd = future_to_cmd[future]
-            try:
-                result = future.result()
-                print(f"Result: '{result}' from '{cmd}'")
-            except Exception as exc:
-                print(f"Command '{cmd}' generated an exception: {exc}")
+    # # Using ThreadPoolExecutor to manage concurrent execution
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=args.max_workers) as executor:
+    #     # Map commands to future tasks
+    #     future_to_cmd = {executor.submit(run_command, cmd): cmd for cmd in commands}
+    #     # As each command completes, print its result
+    #     for future in concurrent.futures.as_completed(future_to_cmd):
+    #         cmd = future_to_cmd[future]
+    #         try:
+    #             result = future.result()
+    #             print(f"Result: '{result}' from '{cmd}'")
+    #         except Exception as exc:
+    #             print(f"Command '{cmd}' generated an exception: {exc}")
 
