62598098e1430ea0efe15e06e18aa96dc232c7c1
Oliver Harold <oliverhsj@hotmail.dk>
2024-05-12 14:46:50 +0700

    Update ppo_rnd_gridworld_unified.py

--- cleanrl/ppo_rnd_gridworld_unified.py
+++ cleanrl/ppo_rnd_gridworld_unified.py
@@ -1003,8 +1003,7 @@ if __name__ == "__main__":
             b_obs_subset = b_obs[b_inds[:16]]
             if args.track and len(b_obs_subset) > 0:
                 # Assuming b_obs_subset is a tensor
-                _, raw_m, att = template.net.get_mask(b_obs_subset, full_output=True)
-                print(raw_m.shape, att.shape)
+                _, att, raw_m = template.net.get_mask(b_obs_subset, full_output=True)
                 m = (raw_m * att[...,None]).sum(1, keepdim=True)
                 m = F.interpolate(m, obs_shape[-2:])
                 raw_m = raw_m.mean(dim=1, keepdim=True)
