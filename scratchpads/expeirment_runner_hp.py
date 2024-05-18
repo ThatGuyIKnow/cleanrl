0f1cb07c5d49920a4b4905453e2276c31716b197
Oliver Harold <oliverhsj@hotmail.dk>
2024-05-18 15:53:46 +0700

    Update expeirment_runner_hp.py

--- scratchpads/expeirment_runner_hp.py
+++ scratchpads/expeirment_runner_hp.py
@@ -40,7 +40,7 @@ args = tyro.cli(Args)
 env_ids_and_tags = [
     # ('Visual/DoorKey5x5-Gridworld-v0' + ' --total-timesteps 2000000 --int-coef 1.0 --update_epochs 8', 'doorkey5x5'),
     # ('Visual/DoorKey6x6-Gridworld-v0' + ' --total-timesteps 3000000 --int-coef 1.0 --update_epochs 8', 'doorkey6x6'),
-    ('Visual/DoorKey8x8-Gridworld-v0' + ' --total-timesteps 1500000 --int-coef 0.000001', 'doorkey8x8'),
+    ('Visual/DoorKey8x8-Gridworld-v0' + ' --total-timesteps 1500000 --int-coef 0.000001 --num_steps 512', 'doorkey8x8'),
     # ('Visual/DoorKey16x16-Gridworld-v0' + ' --total-timesteps 5000000 --int-coef 0.000001', 'doorkey16x16'),
     # ('Visual/MultiRoomS4N2-Gridworld-v0'  + ' --total-timesteps 2000000', 'multiroomS4N2'),
     # ('Visual/MultiRoomS5N4-Gridworld-v0'  + ' --total-timesteps 3000000 --int-coef 0.0005', 'multiroomS5N4'),
@@ -152,9 +152,9 @@ if __name__ == '__main__':
                                   [None,]*args.repeats, 
                                   list(args.seed+i for i in range(args.repeats)), 
                                   '--seed'))
-    ent_coef = [0.01, 0.005, 0.0005]
-    lr = [1e-4, 5e-5, 1e-5]
-    vf = [0.3, 0.5]
+    ent_coef = [0.005, ]
+    lr = [5e-6, 1e-5]
+    vf = [0.3,]
     all_options.append(Option('entropy coef', [None, ] * len(ent_coef), ent_coef, '--ent-coef'))
     all_options.append(Option('learning rate', [None, ] * len(lr), lr, '--learning-rate'))
     all_options.append(Option('Value Func', [None, ] * len(vf), vf, '--vf-coef'))
