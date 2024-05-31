b28bd87e60bd72330a21f7eb670cbd5139df2689
Oliver Harold <oliverhsj@hotmail.dk>
2024-05-31 13:48:57 +0700

    Update expeirment_runner_hp.py

--- scratchpads/expeirment_runner_hp.py
+++ scratchpads/expeirment_runner_hp.py
@@ -10,7 +10,7 @@ import tyro
 class Args:
     track: bool = False
     """track the experiment"""
-    wandb_project_name: str = 'gridworld-rnd-v2-unified-mrn5s6-no-mask2'
+    wandb_project_name: str = 'gridworld-rnd-v2-mrn5s6-no-template'
     """track the experiment"""
     repeats: int = 10
     """number of times to repeat"""
@@ -44,7 +44,7 @@ env_ids_and_tags = [
     # ('Visual/DoorKey16x16-Gridworld-v0' + ' --total-timesteps 5000000 --int-coef 0.000001', 'doorkey16x16'),
     # ('Visual/MultiRoomS4N2-Gridworld-v0'  + ' --total-timesteps 2000000', 'multiroomS4N2'),
     # ('Visual/MultiRoomS5N4-Gridworld-v0'  + ' --total-timesteps 3000000 --int-coef 0.0005  --update_epochs 8', 'multiroomS5N4'),
-    ('Visual/MultiRoomS5N6-Gridworld-v0'  + ' --total-timesteps 6000000 --int-coef 0.0005  --update_epochs 8 --update_proportion 0.15', 'multiroomS5N6'),
+    ('Visual/MultiRoomS5N6-Gridworld-v0'  + ' --total-timesteps 6000000 --int-coef 0.05 --no-use-template --update_epochs 8 --update_proportion 0.15', 'multiroomS5N6'),
     # ('Visual/MultiRoomS10N6-Gridworld-v0'  + ' --total-timesteps 8000000 --int-coef 0.0005  --update_epochs 8', 'multiroomS10N6'),
 ]
 
