8488f0926bbecfea2f4dc0cf39498dc7cd8a7a5e
Oliver Harold <oliverhsj@hotmail.dk>
2024-06-02 17:17:58 +0700

    Update expeirment_runner_hp.py

--- scratchpads/expeirment_runner_hp.py
+++ scratchpads/expeirment_runner_hp.py
@@ -10,7 +10,7 @@ import tyro
 class Args:
     track: bool = False
     """track the experiment"""
-    wandb_project_name: str = 'gridworld-rnd-v2-doorkey8x8'
+    wandb_project_name: str = 'gridworld-rnd-v2-doorkey8x8-rep'
     """track the experiment"""
     repeats: int = 10
     """number of times to repeat"""
