1c094fb82e7d500a8887b32245145e48c1d0046e
Oliver Harold <oliverhsj@hotmail.dk>
2024-06-08 20:21:46 +0700

    Update ppo_rnd_gridworld_unified.py

--- cleanrl/ppo_rnd_gridworld_unified.py
+++ cleanrl/ppo_rnd_gridworld_unified.py
@@ -31,6 +31,36 @@ from torch import Tensor
 from typing import Optional, Tuple, Union
 
 
+class MaskedRunningMeanStd(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float64')
+        self.var = np.ones(shape, 'float64')
+        self.count = np.ones(shape, 'float64') * epsilon
+
+    def update(self, x, mask):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = mask.sum(axis=0)
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count)
+
+def update_mean_var_count_from_moments(mean, var, count, batch_mean, batch_var, batch_count):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
+
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + np.square(delta) * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
+
+    return new_mean, new_var, new_count
+
 
 class BaseNetwork(nn.Module):
     def __init__(self):
